dec92c01da0215cc0b88c1da51c82ec318066f6d
Raquel Lira <raquel.lira@gmail.com>
2016-08-25 19:34:49 -0300

    First Application Configuration

---
+++ src/main/java/com/concretesolutions/VigilantOctoBroccoliApplication.java
@@ -0,0 +1,12 @@
+package com.concretesolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VigilantOctoBroccoliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VigilantOctoBroccoliApplication.class, args);
+	}
+}
