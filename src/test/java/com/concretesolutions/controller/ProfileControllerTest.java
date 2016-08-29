23a27c90438cf48f2e089a395ff60fbdf0a3dc39
Raquel Lira <raquel.lira@gmail.com>
2016-08-29 06:43:08 -0300

    Created test stubs

---
+++ src/test/java/com/concretesolutions/controller/ProfileControllerTest.java
@@ -0,0 +1,10 @@
+package com.concretesolutions.controller;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by raquel on 8/29/16.
+ */
+public class ProfileControllerTest {
+
+}
---
+++ src/test/java/com/concretesolutions/service/ProfileServiceTest.java
@@ -0,0 +1,10 @@
+package com.concretesolutions.service;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by raquel on 8/29/16.
+ */
+public class ProfileServiceTest {
+
+}
